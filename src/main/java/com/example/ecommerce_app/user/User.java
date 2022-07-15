1654078c37d8645a6ea1c6f04913ae9baeeef49c
Vince <vinks59@gmail.com>
2022-07-15 12:26:26 -0500

    created user domain class

---
+++ src/main/java/com/example/ecommerce_app/user/User.java
@@ -0,0 +1,75 @@
+package com.example.ecommerce_app.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password_hash;
+    private String password_salt;
+    private String email;
+
+    public User() {
+    }
+
+    public User(Long id, String username, String password_hash, String password_salt, String email) {
+        this.id = id;
+        this.username = username;
+        this.password_hash = password_hash;
+        this.password_salt = password_salt;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword_hash() {
+        return password_hash;
+    }
+
+    public void setPassword_hash(String password_hash) {
+        this.password_hash = password_hash;
+    }
+
+    public String getPassword_salt() {
+        return password_salt;
+    }
+
+    public void setPassword_salt(String password_salt) {
+        this.password_salt = password_salt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User [email=" + email + ", id=" + id + ", username=" + username + "]";
+    }
+
+}
