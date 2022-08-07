15e313bf7530977d6839a27efb1cb9051c39cf28
Vince <vinks59@gmail.com>
2022-08-07 09:25:43 -0500

    change findByUsername to findFirstByUsername

--- src/main/java/com/example/ecommerce_app/user/ApplicationUserRepository.java
+++ src/main/java/com/example/ecommerce_app/user/ApplicationUserRepository.java
@@ -1,11 +1,12 @@
 package com.example.ecommerce_app.user;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ApplicationUserRepository extends CrudRepository<ApplicationUser, Long> {
-    List<ApplicationUser> findByUsername(String username);
+    Optional<ApplicationUser> findFirstByUsername(String username);
 }
