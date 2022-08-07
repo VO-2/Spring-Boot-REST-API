123cbc66379ec8f1ac7f4fcc208f158156e75bad
Vince <vinks59@gmail.com>
2022-08-07 09:24:49 -0500

    init

---
+++ src/test/java/com/example/ecommerce_app/user/UserTestConfig.java
@@ -0,0 +1,23 @@
+package com.example.ecommerce_app.user;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserTestConfig {
+
+    @Bean
+    private ApplicationUser sampleApplicationUser1() {
+        return new ApplicationUser(
+            "username",
+            "password",
+            "email@email.com",
+            true,
+            true,
+            true,
+            true,
+            List.of());
+    }
+}
