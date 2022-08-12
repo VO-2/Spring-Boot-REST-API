90f7d0c063241022c0b4b3da1ac969d5fd43b1c1
Vince <vinks59@gmail.com>
2022-08-12 16:39:22 -0500

    Add Transactional annotation

--- src/main/java/com/example/ecommerce_app/user/ApplicationUserService.java
+++ src/main/java/com/example/ecommerce_app/user/ApplicationUserService.java
@@ -1,5 +1,7 @@
 package com.example.ecommerce_app.user;
 
+import javax.transaction.Transactional;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ public class ApplicationUserService implements UserDetailsService {
     }
 
     @Override
+    @Transactional
     public ApplicationUser loadUserByUsername(String username) throws UsernameNotFoundException {
         return applicationUserRepository.findFirstByUsername(username)
             .orElseThrow(() -> new UsernameNotFoundException(String.format("ApplicationUser '%s' not found", username)));
