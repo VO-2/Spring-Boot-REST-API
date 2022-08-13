1bba05b31c1b6dab694ba73710539615d65eb9a0
Vince <vinks59@gmail.com>
2022-08-13 11:46:42 -0500

    init

---
+++ src/main/java/com/example/ecommerce_app/product/Category.java
@@ -0,0 +1,30 @@
+package com.example.ecommerce_app.product;
+
+public enum Category {
+    CLOTHES ("Clothing, Shoes, Jewelry & Watches"),
+    BOOKS ("Books"),
+    MEDIA ("Movies, Music & Games"),
+    ELECTRONICS ("Electronics"),
+    COMPUTERS ("Computers"),
+    SMART_HOME ("Smart Home"),
+    HOUSE_SUPPLIES ("Home, Garden & Tools"),
+    PET_SUPPLIES ("Pet Supplies"),
+    FOOD ("Food & Grocery"),
+    BEAUTY_HEALTH ("Beauty & Health"),
+    Toys ("Toys, Kids & Baby"),
+    HANDMANDE ("Handmade"),
+    SPORTS ("Sports"),
+    OUTDOORS ("Outdoors"),
+    AUTOMOTIVE ("Automotive & Industrial"),
+    SCIENTIFIC ("Industrial and Scientific");
+
+    private final String title;
+
+    Category(String title) {
+        this.title=title;
+    }
+
+    String title() {
+        return this.title;
+    }
+}
