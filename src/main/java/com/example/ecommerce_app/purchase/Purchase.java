15f4111f14deca0a96c1bbcab26b3561c9c6b399
Vince <vinks59@gmail.com>
2022-09-01 15:35:33 -0400

    add constructor, getters, setters

--- src/main/java/com/example/ecommerce_app/purchase/Purchase.java
+++ src/main/java/com/example/ecommerce_app/purchase/Purchase.java
@@ -2,13 +2,13 @@ package com.example.ecommerce_app.purchase;
 
 import java.time.LocalDate;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 
 import com.example.ecommerce_app.product.Product;
 import com.example.ecommerce_app.user.ApplicationUser;
@@ -28,7 +28,55 @@ public class Purchase {
     @JoinColumn(name = "user_id")
     private ApplicationUser purchaser;
 
-    @Column(nullable = false)
+    @NotNull
     private LocalDate purchase_date;
 
+    public Purchase() {
+    }
+
+    public Purchase(Product product, ApplicationUser purchaser, @NotNull LocalDate purchase_date) {
+        this.product = product;
+        this.purchaser = purchaser;
+        this.purchase_date = purchase_date;
+    }
+
+    public Purchase(Long purchase_id, Product product, ApplicationUser purchaser, @NotNull LocalDate purchase_date) {
+        this.purchase_id = purchase_id;
+        this.product = product;
+        this.purchaser = purchaser;
+        this.purchase_date = purchase_date;
+    }
+
+    public Long getPurchase_id() {
+        return purchase_id;
+    }
+
+    public void setPurchase_id(Long purchase_id) {
+        this.purchase_id = purchase_id;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public ApplicationUser getPurchaser() {
+        return purchaser;
+    }
+
+    public void setPurchaser(ApplicationUser purchaser) {
+        this.purchaser = purchaser;
+    }
+
+    public LocalDate getPurchase_date() {
+        return purchase_date;
+    }
+
+    public void setPurchase_date(LocalDate purchase_date) {
+        this.purchase_date = purchase_date;
+    }
+    
 }
