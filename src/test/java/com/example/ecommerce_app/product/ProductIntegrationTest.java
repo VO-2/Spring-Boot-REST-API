eb6d6deb7f28a1952c503fc17d8c3b85d0a940b6
Vince <vinks59@gmail.com>
2022-09-01 15:36:30 -0400

    init

---
+++ src/test/java/com/example/ecommerce_app/product/ProductIntegrationTest.java
@@ -0,0 +1,89 @@
+package com.example.ecommerce_app.product;
+
+import static com.example.ecommerce_app.product.ProductTestConfig.testProduct;
+import static com.example.ecommerce_app.product.ProductTestConfig.testProductToDelete;
+import static com.example.ecommerce_app.product.ProductTestConfig.testProductToRead;
+import static com.example.ecommerce_app.product.ProductTestConfig.testProductViolatingNameNullConstraint;
+import static com.example.ecommerce_app.product.ProductTestConfig.testProductViolatingNameSizeConstraint;
+import static com.example.ecommerce_app.product.ProductTestConfig.unusedProductId;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductIntegrationTest {
+
+    @Autowired
+    private ProductController productController;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @BeforeAll
+    void init() {
+        productRepository.save(testProductToDelete);
+        productRepository.save(testProductToRead);
+    }
+
+    @Test
+    void testCreateProduct() {
+        assertDoesNotThrow(() -> productController.createProduct(testProduct));
+    }
+
+    @Test
+    void testDeleteProduct() {
+        assertDoesNotThrow(() -> productController.deleteProduct(testProductToDelete.getProduct_id()));
+    }
+
+    @Test
+    void testGetAllProducts() {
+        assertDoesNotThrow(() -> productController.getAllProducts());
+    }
+
+    @Test
+    void testGetProduct() {
+        assertDoesNotThrow(() -> productController.getProduct(testProductToRead.getProduct_id()));
+    }
+
+    @Test
+    void testUpdateProduct() {
+        assertDoesNotThrow(() -> productController.updateProduct(testProductToRead));
+    }
+
+    @Test
+    void createProductExceptionOnInvalidId() {
+        assertThrows(
+            RuntimeException.class,
+            () -> productController.getProduct(unusedProductId)
+        );
+    }
+
+    @Test
+    void deleteProductExceptionOnInvalidId() {
+        assertThrows(
+            RuntimeException.class,
+            () -> productController.deleteProduct(unusedProductId)
+        );
+    }
+
+    @Test
+    void createProductExceptionOnInvalidNameLength() {
+        assertThrows(
+            RuntimeException.class,
+            () -> productController.createProduct(testProductViolatingNameSizeConstraint)
+        );
+    }
+    
+    @Test
+    void createProductExceptionOnNullName() {
+        assertThrows(
+            RuntimeException.class,
+            () -> productController.createProduct(testProductViolatingNameNullConstraint)
+        );
+    }
+
+}
