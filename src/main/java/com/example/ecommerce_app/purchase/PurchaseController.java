72d52fe1107d4a55584a49e4b4b701a157a64288
Vince <vinks59@gmail.com>
2022-09-01 17:08:04 -0400

    add class level validated

--- src/main/java/com/example/ecommerce_app/purchase/PurchaseController.java
+++ src/main/java/com/example/ecommerce_app/purchase/PurchaseController.java
@@ -3,6 +3,7 @@ package com.example.ecommerce_app.purchase;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 @RequestMapping("/api/v1/purchase")
 public class PurchaseController {
 
