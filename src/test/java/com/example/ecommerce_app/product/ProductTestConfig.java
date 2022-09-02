3b50c198d737b6a4f3531788b2fc1aaa4f27eb78
Vince <vinks59@gmail.com>
2022-09-01 20:05:28 -0400

    add sampleblob, remove ApplicationUser dependency from Products

--- src/test/java/com/example/ecommerce_app/product/ProductTestConfig.java
+++ src/test/java/com/example/ecommerce_app/product/ProductTestConfig.java
@@ -1,9 +1,13 @@
 package com.example.ecommerce_app.product;
 
 import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.Properties;
 import java.util.Set;
 
 import com.example.ecommerce_app.user.ApplicationUser;
+import com.mysql.cj.exceptions.ExceptionInterceptorChain;
+import com.mysql.cj.log.NullLogger;
 
 public class ProductTestConfig {
     
@@ -20,6 +24,10 @@ public class ProductTestConfig {
         Set.of()
     );
 
+    static final Blob sampleBlob = new com.mysql.cj.jdbc.Blob(new byte[] {0},
+        new ExceptionInterceptorChain(null, new Properties(), new NullLogger(""))
+    );
+
     static final Product testProduct = new Product(
         "name",
         1,
@@ -27,8 +35,8 @@ public class ProductTestConfig {
         BigDecimal.ONE,
         "description",
         Set.of(),
-        null,
-        sampleApplicationUser
+        sampleBlob,
+        null
     );
 
     static final Product testProductToRead = new Product(
@@ -38,20 +46,19 @@ public class ProductTestConfig {
         BigDecimal.ONE,
         "description",
         Set.of(),
-        null,
-        sampleApplicationUser
+        sampleBlob,
+        null
     );
 
     static final Product testProductToDelete = new Product(
-        100l,
         "name",
         1,
         true,
         BigDecimal.ONE,
         "description",
         Set.of(),
-        null,
-        sampleApplicationUser
+        sampleBlob,
+        null
     );
 
     static final Product testProductViolatingNameSizeConstraint = new Product(
@@ -61,8 +68,8 @@ public class ProductTestConfig {
         BigDecimal.ONE,
         "description",
         Set.of(),
-        null,
-        sampleApplicationUser
+        sampleBlob,
+        null
     );
 
     static final Product testProductViolatingNameNullConstraint = new Product(
@@ -72,8 +79,8 @@ public class ProductTestConfig {
         BigDecimal.ONE,
         "description",
         Set.of(),
-        null,
-        sampleApplicationUser
+        sampleBlob,
+        null
     );
 
     static final long unusedProductId = 50l;
