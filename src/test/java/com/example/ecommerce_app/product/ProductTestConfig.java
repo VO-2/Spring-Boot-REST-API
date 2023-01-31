ac9feb377fe916aea19c08b0dd20e5754dd891ce
Vince <vinks59@gmail.com>
2023-01-31 09:12:40 -0500

    remove unused ApplicationUser object

--- src/test/java/com/example/ecommerce_app/product/ProductTestConfig.java
+++ src/test/java/com/example/ecommerce_app/product/ProductTestConfig.java
@@ -5,24 +5,10 @@ import java.sql.Blob;
 import java.util.Properties;
 import java.util.Set;
 
-import com.example.ecommerce_app.user.ApplicationUser;
 import com.mysql.cj.exceptions.ExceptionInterceptorChain;
 import com.mysql.cj.log.NullLogger;
 
 public class ProductTestConfig {
-    
-    static final ApplicationUser sampleApplicationUser = new ApplicationUser(
-        "username",
-        "password",
-        "email@email.com",
-        true,
-        true,
-        true,
-        true,
-        Set.of(),
-        Set.of(),
-        Set.of()
-    );
 
     static final Blob sampleBlob = new com.mysql.cj.jdbc.Blob(new byte[] {0},
         new ExceptionInterceptorChain(null, new Properties(), new NullLogger(""))
