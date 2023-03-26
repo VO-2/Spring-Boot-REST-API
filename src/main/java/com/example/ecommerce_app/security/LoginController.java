afb74931af9ba8a38764af1a13badd17c5b51f11
Vince <vinks59@gmail.com>
2023-03-26 10:33:26 -0400

    change login to be mapped to POST instead of GET

--- src/main/java/com/example/ecommerce_app/security/LoginController.java
+++ src/main/java/com/example/ecommerce_app/security/LoginController.java
@@ -3,6 +3,7 @@ package com.example.ecommerce_app.security;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,7 +20,7 @@ public class LoginController {
         this.loginService = loginService;
     }
 
-    @GetMapping
+    @PostMapping
     public ApplicationUser login(@RequestBody ApplicationUser user) {
         return loginService.login(user);
     }
